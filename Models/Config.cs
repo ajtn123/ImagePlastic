69c0ff0091cb45f1ae2243c121118781753a7605
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-03-15 00:38:30 +0800

    bump dependencies

--- Models/Config.cs
+++ Models/Config.cs
@@ -9,9 +9,10 @@ namespace ImagePlastic.Models;
 public class Config
 {
     public FileInfo? DefaultFile { get; set; }
+    public string BackgroundColor { get; set; } = "#00ABCDEF";
+    //Transparent window background color effect.
     public Transparency[] Blurs { get; set; }
         = [Transparency.AcrylicBlur, Transparency.Mica, Transparency.Blur, Transparency.Transparent];
-    public string BackgroundColor { get; set; } = "#00ABCDEF";
     public string[] Extensions { get; set; }
         = [".png", ".jpg", ".jpeg", ".webp", ".avif", ".heic", ".heif", ".bmp", ".jxl", ".gif", ".psd", ".svg"];
     public bool ExtendImageToTitleBar { get; set; } = true;
@@ -19,15 +20,20 @@ public class Config
     public byte SystemAccentColorOpacity { get; set; } = 127;
     public string CustomAccentColor { get; set; } = "#7FABCDEF";
     public bool LoadingIndicator { get; set; } = true;
+    //Not working.
     public StretchMode Stretch { get; set; } = StretchMode.Uniform;
     public bool DeleteConfirmation { get; set; } = true;
     public bool RenameConfirmation { get; set; } = false;
     public bool MoveConfirmation { get; set; } = false;
+    //Show images in the current directory and all its subdirectories, including their own subdirectories, recursively;
+    //Enable Recursive in the context menu for single use.
     public bool RecursiveSearch { get; set; } = false;
     public bool ShowHiddenOrSystemFile { get; set; } = false;
+    //Preload bitmaps.
     public bool Preload { get; set; } = true;
     public int PreloadLeft { get; set; } = 0;
     public int PreloadRight { get; set; } = 2;
+    //Anti-aliasing mode.
     public BitmapInterpolationMode InterpolationMode { get; set; }
         = BitmapInterpolationMode.HighQuality;
     public ButtonName PanButton { get; set; } = ButtonName.Left;
@@ -36,9 +42,12 @@ public class Config
     public Dictionary<MagickFormat, string?> EditApp = new(){
         {default,@"C:\Program Files\GIMP 2\bin\gimp-2.10.exe"},
     };
+    //Left and right arrows.
     public ArrowButton ArrowButton { get; set; } = ArrowButton.Normal;
+    //For ArrowButton.Normal.
     public int ArrowSize { get; set; } = 48;
     public bool ColorPickerCopy { get; set; } = true;
+    //The square at the center of the ColorPicker magnifier, indicating the pixel where the color is picked.
     public bool ColorPickerAiming { get; set; } = true;
     public PathQuotation PathCopyQuotation { get; set; } = PathQuotation.ContainSpace;
     //Close the last child window, if exist, before closing the parent.
