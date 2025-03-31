5b3dc2445fdc3a011052ce1199638335904adcd9
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-03-31 12:31:15 +0800

    fix

--- Utilities/Draggable.cs
+++ Utilities/Draggable.cs
@@ -10,10 +10,9 @@ public static class DraggableBehavior
 {
     public static readonly AttachedProperty<bool> IsDraggableProperty
         = AvaloniaProperty.RegisterAttached<Control, bool>("IsDraggable", typeof(DraggableBehavior));
-    public static readonly AttachedProperty<bool> IsMouseDownProperty
-        = AvaloniaProperty.RegisterAttached<Control, bool>("IsMouseDown", typeof(DraggableBehavior));
-    public static readonly AttachedProperty<PointerPoint> OriginalPointProperty
-        = AvaloniaProperty.RegisterAttached<Control, PointerPoint>("OriginalPoint", typeof(DraggableBehavior));
+
+    private static bool IsMouseDown;
+    private static PointerPoint OriginalPoint;
 
     public static bool GetIsDraggable(Control control)
         => control.GetValue(IsDraggableProperty);
@@ -28,8 +27,8 @@ public static class DraggableBehavior
             control.PointerCaptureLost += Control_PointerCaptureLost;
             if (control is Window)
                 control.LostFocus += Control_LostFocus;
-            else if (control.FindAncestorOfType<Window>() is Window window2)
-                window2.LostFocus += Control_LostFocus;
+            else if (control.FindAncestorOfType<Window>() is Window window)
+                window.LostFocus += Control_LostFocus;
         }
         else
         {
@@ -39,29 +38,20 @@ public static class DraggableBehavior
             control.PointerCaptureLost -= Control_PointerCaptureLost;
             if (control is Window)
                 control.LostFocus -= Control_LostFocus;
-            else if (control.FindAncestorOfType<Window>() is Window window2)
-                window2.LostFocus -= Control_LostFocus;
+            else if (control.FindAncestorOfType<Window>() is Window window)
+                window.LostFocus -= Control_LostFocus;
         }
     }
-    private static void SetIsMouseDown(Control control, bool value)
-    => control.SetValue(IsMouseDownProperty, value);
-    private static bool GetIsMouseDown(Control control)
-        => control.GetValue(IsMouseDownProperty);
-
-    private static void SetOriginalPoint(Control control, PointerPoint value)
-        => control.SetValue(OriginalPointProperty, value);
-    private static PointerPoint GetOriginalPoint(Control control)
-        => control.GetValue(OriginalPointProperty);
 
     //https://github.com/AvaloniaUI/Avalonia/discussions/8441#discussioncomment-3081536
     private static void Control_PointerMoved(object? sender, PointerEventArgs e)
     {
-        if (sender is not Control control || !GetIsMouseDown(control)) return;
+        if (sender is not Control control || !IsMouseDown) return;
         Window? window = control as Window ?? control.FindAncestorOfType<Window>();
         if (window != null)
         {
             PointerPoint currentPoint = e.GetCurrentPoint(window);
-            PointerPoint originalPoint = GetOriginalPoint(control);
+            PointerPoint originalPoint = OriginalPoint;
             window.Position = new PixelPoint(
                 window.Position.X + (int)(currentPoint.Position.X - originalPoint.Position.X),
                 window.Position.Y + (int)(currentPoint.Position.Y - originalPoint.Position.Y)
@@ -74,15 +64,15 @@ public static class DraggableBehavior
         Window? window = control as Window ?? control.FindAncestorOfType<Window>();
         if (window != null && window.WindowState == WindowState.Normal && e.GetCurrentPoint(window).Properties.IsLeftButtonPressed)
         {
-            SetIsMouseDown(control, true);
-            SetOriginalPoint(control, e.GetCurrentPoint(window));
+            IsMouseDown = true;
+            OriginalPoint = e.GetCurrentPoint(window);
         }
     }
 
     private static void Control_PointerReleased(object? sender, PointerReleasedEventArgs e)
-    { if (sender is Control control) SetIsMouseDown(control, false); }
+        => IsMouseDown = false;
     private static void Control_PointerCaptureLost(object? sender, PointerCaptureLostEventArgs e)
-    { if (sender is Control control) SetIsMouseDown(control, false); }
+        => IsMouseDown = false;
     private static void Control_LostFocus(object? sender, RoutedEventArgs e)
-    { if (sender is Control control) SetIsMouseDown(control, false); }
+        => IsMouseDown = false;
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -130,33 +130,6 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         ImageNavigationOffset += offset;
     }
 
-
-    ////Make entire window draggable.
-    ////https://github.com/AvaloniaUI/Avalonia/discussions/8441#discussioncomment-3081536
-    //private bool _mouseDownForWindowMoving = false;
-    //private PointerPoint _originalPoint;
-    //private void Window_PointerMoved(object? sender, PointerEventArgs e)
-    //{
-    //    if (!_mouseDownForWindowMoving) return;
-    //    PointerPoint currentPoint = e.GetCurrentPoint(this);
-    //    Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
-    //        Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
-    //}
-    //private void Window_PointerPressed(object? sender, PointerPressedEventArgs e)
-    //{
-    //    var point = e.GetCurrentPoint(sender as Control);
-    //    if (!point.Properties.IsLeftButtonPressed) return;
-    //    if (WindowState == WindowState.Maximized || WindowState == WindowState.FullScreen) return;
-    //    _mouseDownForWindowMoving = true;
-    //    _originalPoint = e.GetCurrentPoint(this);
-    //}
-    //private void Window_PointerReleased(object? sender, PointerReleasedEventArgs e)
-    //    => _mouseDownForWindowMoving = false;
-    //private void Window_PointerCaptureLost(object? sender, PointerCaptureLostEventArgs e)
-    //    => _mouseDownForWindowMoving = false;
-    //private void Window_LostFocus(object? sender, RoutedEventArgs e)
-    //    => _mouseDownForWindowMoving = false;
-
     //Auto resize Title Bar.
     private void Window_SizeChanged(object? sender, SizeChangedEventArgs e)
     {
