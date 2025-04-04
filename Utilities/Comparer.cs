b0e8bf3b6593110cba3a76f3b52121240b66443f
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-04-04 20:26:19 +0800

    MO.

--- Utilities/Comparer.cs
+++ Utilities/Comparer.cs
@@ -6,10 +6,10 @@ using System.Security;
 namespace ImagePlastic.Utilities;
 
 //https://stackoverflow.com/a/54812587
-[SupportedOSPlatform("windows7.0")]
-[SuppressUnmanagedCodeSecurity]
 public partial class IntuitiveStringComparer : IComparer<string>
 {
+    [SuppressUnmanagedCodeSecurity]
+    [SupportedOSPlatform("windows7.0")]
     [LibraryImport("shlwapi.dll", StringMarshalling = StringMarshalling.Utf16)]
     private static partial int StrCmpLogicalW(string psz1, string psz2);
     public int Compare(string? x, string? y)
